f3bd074d8988376f2cb58f828014c2b77df76b7d
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-09-06 14:31:16 -0600

    Push Base Conectada

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,52 @@
 #include <typeinfo>
 #include <string>
 #include <fstream>
+#include <sqlite3.h>
 
 using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(){
-	
+int main(int argc, char** argv) {
+	sqlite3 *conn;
+	sqlite3_stmt *res;
+	int error=0;
+	int rec_count =0;
+	const char *tail;
+	while(true){
+		int opcion;
+		cout<<"1- Listar\n2- Agregar\n3- Eliminar\n4- Listar Jefe y Empleado\n5- Listar Salarb\n6- Empleados de departamento\n7- Salir\nIngrese opcion: ";
+		cin>>opcion;
+		switch(opcion){
+			case 1:{
+				
+				break;
+			}
+			case 2:{
+				
+				break;
+			}
+			case 3:{
+				
+				break;
+			}
+			case 4:{
+				
+				break;
+			}
+			case 5:{
+				
+				break;
+			}
+			case 6:{
+				
+				break;
+			}
+			case 7:{
+				exit(0);
+				break;
+			}
+		}
+	}
+	system("pause");
+	return 0;
 }
